67126a8f788df4f221a991d37ceb875043d4f33d
ruzs <eddie10913@gmail.com>
2023-02-16 09:53:03 +0800

    完成最新消息及購物流程功能

---
+++ front/news.php
@@ -0,0 +1,12 @@
+<h2 class="ct">最新消息</h2>
+<table class="all">
+  <tr class="tt">
+    <td class="ct">標題</td>
+  </tr>
+  <tr>
+    <td class="pp">年終特賣會開跑了</td>
+  </tr>
+  <tr>
+    <td class="pp">情人節特惠活動</td>
+  </tr>
+</table>
