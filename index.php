59e0f66a3e48056c71e719c863876d82d64705a5
ruzs <eddie10913@gmail.com>
2023-02-16 15:08:55 +0800

    完成登出功能

--- index.php
+++ index.php
@@ -25,6 +25,7 @@
         <a href="?do=look">購物流程</a> |
         <a href="?do=buycart">購物車</a> |
         <a href="?do=login">會員登入</a> |
+        <a href="./api/logout.php?table=mem">登出</a> |
         <a href="?do=admin">管理登入</a>
       </div>
       <marquee>情人節特惠活動 &nbsp; 為了慶祝七夕情人節，將舉辦情人兩人到現場有七七折之特惠活動~</marquee>
