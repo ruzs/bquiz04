cbf9e55780b44bdc87a602fba35d16f0e7cb6daf
ruzs <eddie10913@gmail.com>
2023-02-16 16:19:08 +0800

    完成新增與修改管理員功能

--- back/admin.php
+++ back/admin.php
@@ -16,8 +16,8 @@
       <?php
         if ($row['acc'] != 'admin') {
       ?>
-      <button onclick="location.href='?do=edit_admin&id=<?=$row['id'];?>"></button>
-      <button onclick="del('admin',<?=$row['id'];?>)"></button>
+      <button onclick="location.href='?do=edit_admin&id=<?=$row['id'];?>'">修改</button>
+      <button onclick="del('admin',<?=$row['id'];?>)">刪除</button>
       <?php
         } else {
           echo "此帳號為最高權限";
--- back/new_admin.php
+++ back/new_admin.php
@@ -1,5 +1,5 @@
 <h2 class="ct">新增管理帳號</h2>
-<form action="./api/new_admin.php" method="post">
+<form action="./api/save_admin.php" method="post">
   <table class="all">
     <tr>
       <td class="tt ct">帳號</td>
@@ -7,15 +7,21 @@
     </tr>
     <tr>
       <td class="tt ct">密碼</td>
-      <td class="pp"><input type="text" name="pw" id="pw"></td>
+      <td class="pp"><input type="password" name="pw" id="pw"></td>
     </tr>
     <tr>
       <td class="tt ct">權限</td>
-      <td class="pp"><input type="text" name="pr" id="pr"></td>
+      <td class="pp">
+        <div><input type="checkbox" name="pr[]" value="1">商品分類與管理</div>
+        <div><input type="checkbox" name="pr[]" value="2">訂單管理</div>
+        <div><input type="checkbox" name="pr[]" value="3">會員管理</div>
+        <div><input type="checkbox" name="pr[]" value="4">頁尾版權區管理</div>
+        <div><input type="checkbox" name="pr[]" value="5">最新消息管理</div>
+      </td>
     </tr>
   </table>
   <div class="ct">
     <input type="submit" value="新增">
-    <input type="reset" value="reset">
+    <input type="reset" value="重置">
   </div>
 </form>
