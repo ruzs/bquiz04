50572b184e6d3cc51c0b29385599af52f063f3ff
ruzs <eddie10913@gmail.com>
2023-02-20 11:35:27 +0800

    完成商品區導覽列顯示 & debug edit_goods 宣告

--- back/edit_goods.php
+++ back/edit_goods.php
@@ -1,7 +1,10 @@
 <h2 class="ct">新增商品</h2>
 <!-- table.all>tr*9>td.tt.ct+td.pp>input -->
 <form action="./api/save_goods.php" method="post" enctype="multipart/form-data">
-    <table class="all">
+  <?php
+  $row = $Goods->find($_GET['id']);
+  ?>
+  <table class="all">
     <tr>
       <td class="tt ct">所屬大分類</td>
       <td class="pp"><select name="big" id="big"></select></td>
@@ -12,31 +15,31 @@
     </tr>
     <tr>
       <td class="tt ct">商品編號</td>
-      <td class="pp"><?=$row['no'];?></td>
+      <td class="pp"><?= $row['no']; ?></td>
     </tr>
     <tr>
       <td class="tt ct">商品名稱</td>
-      <td class="pp"><input type="text" name="name" value="<?=$row['name'];?>"></td>
+      <td class="pp"><input type="text" name="name" value="<?= $row['name']; ?>"></td>
     </tr>
     <tr>
       <td class="tt ct">商品價格</td>
-      <td class="pp"><input type="number" name="price" value="<?=$row['price'];?>"></td>
+      <td class="pp"><input type="number" name="price" value="<?= $row['price']; ?>"></td>
     </tr>
     <tr>
       <td class="tt ct">規格</td>
-      <td class="pp"><input type="text" name="spec" value="<?=$row['spec'];?>"></td>
+      <td class="pp"><input type="text" name="spec" value="<?= $row['spec']; ?>"></td>
     </tr>
     <tr>
       <td class="tt ct">庫存量</td>
-      <td class="pp"><input type="text" name="stock" value="<?=$row['stock'];?>"></td>
+      <td class="pp"><input type="text" name="stock" value="<?= $row['stock']; ?>"></td>
     </tr>
     <tr>
       <td class="tt ct">商品圖片</td>
-      <td class="pp"><textarea name="intro" cols="60" rows="20"><?=$row['intro'];?></textarea></td>
+      <td class="pp"><textarea name="intro" cols="60" rows="20"><?= $row['intro']; ?></textarea></td>
     </tr>
     <tr>
       <td class="tt ct">商品介紹</td>
-      <td class="pp"><textarea name="intro" cols="60" rows="20"><?=$row['intro'];?></textarea></td>
+      <td class="pp"><textarea name="intro" cols="60" rows="20"><?= $row['intro']; ?></textarea></td>
     </tr>
   </table>
   <div class="ct">
@@ -47,27 +50,33 @@
 </form>
 
 <script>
+  getBigs()
 
-getBigs()
-
-$("#big").on("change",function(){
+  $("#big").on("change", function() {
     getMids();
-})
+  })
 
-let goods={big:<?=$row['big'];?>,mid:<?=$row['mid'];?>}
+  let goods = {
+    big: <?= $row['big']; ?>,
+    mid: <?= $row['mid']; ?>
+  }
 
-function getBigs(){
-    $('#big').load("./api/get_bigs.php",()=>{
-        $(`#big option[value='${goods.big}']`).prop('selected',true)
-        let big=$("#big").val();
-        $("#mid").load("./api/get_mids.php",{big},()=>{
-            $(`#mid option[value='${goods.mid}']`).prop('selected',true)
-        })
+  function getBigs() {
+    $('#big').load("./api/get_bigs.php", () => {
+      $(`#big option[value='${goods.big}']`).prop('selected', true)
+      let big = $("#big").val();
+      $("#mid").load("./api/get_mids.php", {
+        big
+      }, () => {
+        $(`#mid option[value='${goods.mid}']`).prop('selected', true)
+      })
     })
-}
+  }
 
-function getMids(){
-    let big=$("#big").val();
-    $("#mid").load("./api/get_mids.php",{big})
-}
+  function getMids() {
+    let big = $("#big").val();
+    $("#mid").load("./api/get_mids.php", {
+      big
+    })
+  }
 </script>
--- front/main.php
+++ front/main.php
@@ -1,9 +1,28 @@
 <?php
 
 if(isset($_GET['type']) && $_GET['type'] != 0){
-    echo "某分類商品";
+    $type=$Type->find($_GET['type']);
+    if($type['parent']==0){
+        $nav=$type['name'];
+    }else{
+        $type_big=$Type->find($type['parent']);
+        $nav=$type_big['name'] . " > " . $type['name'];
+    }
+
 
 }else{
-    echo "全部商品";
+    $nav='全部商品';
 
 }
+?>
+
+
+<h1><?=$nav;?></h1>
+
+
+<?php
+
+foreach($rows as $row){
+
+}
+?>
