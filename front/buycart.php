e796efcb98423d5d5dd23951247f2a72f96f5276
ruzs <eddie10913@gmail.com>
2023-02-20 14:44:23 +0800

    在購物車頁面顯示使用者選購的商品列表

--- front/buycart.php
+++ front/buycart.php
@@ -8,4 +8,33 @@ if (!isset($_SESSION['mem'])) {
   to("index.php?do=login");
 }
 
-dd($_SESSION['cart']);
+// dd($_SESSION['cart']);
+?>
+<h2 class="ct"><?=$_SESSION['mem']?>的購物車</h2>
+<table class="all">
+  <tr class="tt ct">
+    <td>編號</td>
+    <td>商品名稱</td>
+    <td>數量</td>
+    <td>庫存量</td>
+    <td>單價</td>
+    <td>小計</td>
+    <td>刪除</td>
+  </tr>
+  <?php
+  foreach ($_SESSION['cart'] as $id => $qt) {
+    $row=$Goods->find($id);
+    ?>
+  <tr class="pp ct">
+    <td><?=$row['no'];?></td>
+    <td><?=$row['name'];?></td>
+    <td><?=$qt;?></td>
+    <td><?=$row['stock'];?></td>
+    <td><?=$row['price'];?></td>
+    <td><?=$row['price'] * $qt;?></td>
+    <td><img src="./icon/0415.jpg" alt=""></td>
+  </tr>
+  <?php
+}
+  ?>
+</table>
