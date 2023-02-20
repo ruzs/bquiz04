2d6f37eca738ec96e194e70de4f8ef7929aca7e2
ruzs <eddie10913@gmail.com>
2023-02-20 09:46:23 +0800

    完成顯示分類列表功能

--- back/th.php
+++ back/th.php
@@ -1,59 +1,68 @@
-<?php $bigs=$Type->all(['parent'=>0]);?>
+<?php $bigs = $Type->all(['parent' => 0]); ?>
 
 <h2 class="ct">商品分類</h2>
 <div class="ct">
-    新增大分類
-    <input type="text" name="big" id="big">
-    <button onclick="addType('big')">新增</button>
+  新增大分類
+  <input type="text" name="big" id="big">
+  <button onclick="addType('big')">新增</button>
 </div>
 <div class="ct">
-    新增中分類
-    <select name="b" id="b">
+  新增中分類
+  <select name="b" id="b">
     <?php
-            foreach($bigs as $big){
-                echo "<option value='{$big['id']}'>{$big['name']}</option>";
-            }
-        ?>
-    </select>
-    <input type="text" name="mid" id="mid">
-    <button onclick="addType('mid')">新增</button>
+    foreach ($bigs as $big) {
+      echo "<option value='{$big['id']}'>{$big['name']}</option>";
+    }
+    ?>
+  </select>
+  <input type="text" name="mid" id="mid">
+  <button onclick="addType('mid')">新增</button>
 </div>
 <table class="all">
-<?php
-// $bigs=$Type->all(['parent'=>0]);
-foreach($bigs as $big){
-?>
-<tr class="tt">
-    <td><?=$big['name'];?></td>
-    <td class="ct">
-        <button data-id="<?=$big['id'];?>">修改</button>
-        <button onclick="del('Type',<?=$big['id'];?>)">刪除</button>
-    </td>
-</tr>
-<?php
-}
-?>
-<tr class="pp ct">
-    <td>dsfaf</td>
-    <td>
-        <button>修改</button>
-        <button>刪除</button>
-    </td>
-</tr>
+  <?php
+  // $bigs=$Type->all(['parent'=>0]);
+  foreach ($bigs as $big) {
+  ?>
+    <tr class="tt">
+      <td><?= $big['name']; ?></td>
+      <td class="ct">
+        <button data-id="<?= $big['id']; ?>">修改</button>
+        <button onclick="del('Type',<?= $big['id']; ?>)">刪除</button>
+      </td>
+    </tr>
+    <?php
+    if ($Type->count(['parent' => $big['id']]) > 0) {
+      $mids = $Type->all(['parent' => $big['id']]);
+      foreach ($mids as $mid) {
+    ?>
+        <tr class="pp ct">
+          <td><?= $mid['name']; ?></td>
+          <td>
+            <button data-id="<?= $mid['id']; ?>">修改</button>
+            <button onclick="del('Type',<?= $mid['id']; ?>)">刪除</button>
+          </td>
+        </tr>
+  <?php
+      }
+    }
+  }
+  ?>
 </table>
 
 <script>
-function addType(type){
-    let parent=(type=='big')?0:$("#b").val();
-    let name=(type=='big')?$("#big").val():$("#mid").val()
-    $.post("./api/add_type.php",{parent,name},()=>{
-        location.reload()
+  function addType(type) {
+    let parent = (type == 'big') ? 0 : $("#b").val();
+    let name = (type == 'big') ? $("#big").val() : $("#mid").val()
+    $.post("./api/add_type.php", {
+      parent,
+      name
+    }, () => {
+      location.reload()
     })
-}
-// $.get("./api/get_bigs.php",(bigs)=>{
-//     $("#b").html(bigs);
-// })
-
+  }
+  // $.get("./api/get_bigs.php",(bigs)=>{
+  //     $("#b").html(bigs);
+  // })
 </script>
 
 <h2 class="ct">商品管理</h2>
