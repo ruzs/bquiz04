9b62907fdbc57e479e6bfae98a28cf7e3b70b86f
ruzs <eddie10913@gmail.com>
2023-02-20 10:40:17 +0800

    修改css.css 調整高度,增加overflow設定

--- back.php
+++ back.php
@@ -21,9 +21,9 @@ $pr = unserialize($admin['pr']);
 	<div id="main">
 		<div id="top">
 			<a href="?">
-				<img src="./icon/0416.jpg">
+				<img src="./icon/0416.jpg" style="width:72%;">
 			</a>
-			<img src="./icon/0417.jpg">
+			<img src="./icon/0417.jpg" style="width:25%;">
 		</div>
 		<div id="left" class="ct">
 			<div style="min-height:400px;">
