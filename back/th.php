510c464db22f9236bc9f7de99711c9fe1e79b6a4
ruzs <eddie10913@gmail.com>
2023-02-20 09:17:24 +0800

    建立商品分類畫面

---
+++ back/th.php
@@ -0,0 +1,32 @@
+<h2 class="ct">商品分類</h2>
+<div class="ct">
+    新增大分類
+    <input type="text" name="big" id="">
+    <button>新增</button>
+</div>
+<div class="ct">
+    新增中分類
+    <select name="b" id="b">
+    </select>
+    <input type="text" name="mid" id="mid">
+    <button>新增</button>
+</div>
+<table class="all">
+<tr class="tt">
+    <td>sdsf</td>
+    <td class="ct">
+        <button>修改</button>
+        <button>刪除</button>
+    </td>
+</tr>
+<tr class="pp ct">
+    <td>dsfaf</td>
+    <td>
+        <button>修改</button>
+        <button>刪除</button>
+    </td>
+</tr>
+</table>
+
+
+<h2 class="ct">商品管理</h2>
