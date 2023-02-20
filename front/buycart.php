bad260f4aa7853690c27ed1936b1a64fd0dccb53
ruzs <eddie10913@gmail.com>
2023-02-20 14:26:11 +0800

    建立購物車session,儲存商品資料

--- front/buycart.php
+++ front/buycart.php
@@ -1,6 +1,11 @@
 <?php
-if (isset($_SESSION['mem'])) {
-} else {
+
+if (isset($_GET['id'])) {
+  $_SESSION['cart'][$_GET['id']] = $_GET['qt'];
+}
+
+if (!isset($_SESSION['mem'])) {
   to("index.php?do=login");
 }
-?>
+
+dd($_SESSION['cart']);
