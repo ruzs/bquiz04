4883677fd6e167b7156ef6c9c69f2cba55d35ea5
ruzs <eddie10913@gmail.com>
2023-02-20 09:07:56 +0800

    補充刪除的switch case版本

--- api/del.php
+++ api/del.php
@@ -1,3 +1,17 @@
 <?php include_once "base.php";
 
+/*
+switch($_POST['table']){
+    case "mem":
+        $Mem->del($_POST['id']);
+    break;
+    case 'admin':
+        $Admin->del($_POST['id']);
+    break;
+    case 'ord':
+        $Ord->del($_POST['id']);
+    break;
+}
+ */
+
 ${$_POST['table']}->del($_POST['id']);
