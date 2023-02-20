e8ad4fa32441e34ee9483b7b4ffb00398fb08c52
ruzs <eddie10913@gmail.com>
2023-02-20 15:44:56 +0800

    增加判斷使用者沒有點商品就到購物車的情形

--- front/buycart.php
+++ front/buycart.php
@@ -11,6 +11,13 @@ if (!isset($_SESSION['mem'])) {
 // dd($_SESSION['cart']);
 ?>
 <h2 class="ct"><?=$_SESSION['mem']?>的購物車</h2>
+<?php
+if(!isset($_SESSION['cart'])){
+
+    echo "<h2 class='ct'>目前購物車沒有商品</h2>";
+
+}else{
+?>
 <table class="all">
   <tr class="tt ct">
     <td>編號</td>
@@ -38,3 +45,14 @@ if (!isset($_SESSION['mem'])) {
 }
   ?>
 </table>
+
+<div class="ct">
+    <img src="./icon/0411.jpg" alt="">
+    <img src="./icon/0412.jpg" alt="">
+</div>
+
+
+
+<?php
+}
+?>
