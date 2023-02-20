5bd1b45529061c76da4b3a5fcb4d3871d1e8e87b
ruzs <eddie10913@gmail.com>
2023-02-20 10:20:12 +0800

    完成商品列表頁面及按鈕功能定義

--- back/th.php
+++ back/th.php
@@ -39,7 +39,7 @@
           <td><?= $mid['name']; ?></td>
           <td>
             <button data-id="<?= $mid['id']; ?>" onclick=" editType(this)"">修改</button>
-            <button onclick="del('Type',<?= $mid['id']; ?>)">刪除</button>
+            <button onclick=" del('Type',<?= $mid['id']; ?>)">刪除</button>
           </td>
         </tr>
   <?php
@@ -81,5 +81,38 @@
 
 <h2 class="ct">商品管理</h2>
 <div class="ct">
-    <button onclick="location.href='?do=add_goods'">新增商品</button>
+  <button onclick="location.href='?do=add_goods'">新增商品</button>
 </div>
+
+<table class="all">
+  <tr class="tt ct">
+    <td>編號</td>
+    <td>商品名稱</td>
+    <td>庫存量</td>
+    <td>狀態</td>
+    <td>操作</td>
+  </tr>
+
+
+  <?php
+  $rows = $Goods->all();
+  foreach ($rows as $row) {
+  ?>
+    <tr class="pp ct">
+      <td><?= $row['no']; ?></td>
+      <td><?= $row['name']; ?></td>
+      <td><?= $row['stock']; ?></td>
+      <td><?= ($row['sh'] == 1) ? '販售中' : '已下架'; ?></td>
+      <td>
+        <button onclick="location.href='?do=edit_goods&id=<?= $row['id']; ?>'">修改</button>
+        <button onclick="del('Goods',<?= $row['id']; ?>)">刪除</button>
+        <button onclick="sh('up',<?= $row['id']; ?>)">上架</button>
+        <button onclick="sh('down',<?= $row['id']; ?>)">下架</button>
+
+      </td>
+    </tr>
+  <?php
+  }
+  ?>
+
+</table>
