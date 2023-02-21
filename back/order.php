6a52f36c9c09702ff46a6d4da5c6159a2902a54c
ruzs <eddie10913@gmail.com>
2023-02-21 08:41:48 +0800

    建立訂單管理畫面

---
+++ back/order.php
@@ -0,0 +1,20 @@
+<h2 class="ct">訂單管理</h2>
+<!-- table.all>tr.ct*2>td*6 -->
+<table class="all">
+    <tr class="tt ct">
+        <td>訂單編號</td>
+        <td>金額</td>
+        <td>會員帳號</td>
+        <td>姓名</td>
+        <td>下單時間</td>
+        <td>操作</td>
+    </tr>
+    <tr class="pp ct">
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
